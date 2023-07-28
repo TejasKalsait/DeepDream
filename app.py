ed5dc075846506433a3a3a15235ad91ea9c76422
TejasKalsait <69215615+TejasKalsait@users.noreply.github.com>
2023-07-28 17:23:18 -0500

    smooth 0.0

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ config = {
     "layers_to_use" : ["layer3"],
     "should_display" : False,
     "spatial_shift_size" : 30,
-    "smoothing_coefficient" : 0.4,
+    "smoothing_coefficient" : 0.0,
     "use_noise " : False}
 
 config['dump_dir'] = os.path.join(OUT_IMAGES_PATH, f'{config["model_name"]}_{config["pretrained_weights"]}')
